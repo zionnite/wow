6f9675d72959daae52a6b25b0a64a88cf068a516
Nosakhare Atekha Endurance <nosakharee.16@gmail.com>
2020-12-14 06:33:36 -0800

    changed of homescreen and getting data from server

--- lib/blocs/QouteBloc.dart
+++ lib/blocs/QouteBloc.dart
@@ -4,10 +4,11 @@ import 'package:wow/model/Quote.dart';
 import 'package:wow/services/QuoteService.dart';
 
 class QuoteBloc {
-  final _allQuoteController = StreamController<List<Quote>>();
+  final _allQuoteController = StreamController<List<Quote>>.broadcast();
   Stream<List<Quote>> get allQuoteStream => _allQuoteController.stream;
-  List<Quote> data;
+  StreamSink<List<Quote>> get allQuoteSink => _allQuoteController.sink;
 
+  List<Quote> data;
   QuoteBloc() {
     quotes();
   }
