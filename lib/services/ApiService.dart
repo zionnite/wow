3ae3c927a9b8e74472810055ea1aa951777a94fd
Nosakhare Atekha Endurance <nosakharee.16@gmail.com>
2020-12-27 05:02:50 -0800

    change in file structue

--- lib/services/ApiService.dart
+++ lib/services/ApiService.dart
@@ -6,55 +6,55 @@ import 'package:wow/model/ForumComment.dart';
 import 'package:wow/model/Quote.dart';
 import 'package:http/http.dart' as http;
 
-final String mainUrl = 'http://wow.joons-me.com/Api';
+final String mainUrl = 'http://wow.esuku.xyz/Api';
 final String fakemainUrl = 'http://localhost/wow_php/Api';
 
 Future<List<Quote>> getAllQuotes() async {
-  final response = await http.get('$fakemainUrl/get_quote');
+  final response = await http.get('$mainUrl/get_quote');
   return quoteFromJson(response.body);
 }
 
 Future<List<Quote>> getAllQuotesByPage(int current_page) async {
   final response =
-      await http.get('$fakemainUrl/get_quote_2/' + current_page.toString());
+      await http.get('$mainUrl/get_quote_2/' + current_page.toString());
   return quoteFromJson(response.body);
 }
 
 Future<List<Quote>> getQuoteById(String id) async {
-  final response = await http.get('$fakemainUrl/get_quote_by_id/$id');
+  final response = await http.get('$mainUrl/get_quote_by_id/$id');
   return quoteFromJson(response.body);
 }
 
 Future<List<Category>> getAllCategory() async {
-  final response = await http.get('$fakemainUrl/get_category');
+  final response = await http.get('$mainUrl/get_category');
   return categoryFromJson(response.body);
 }
 
 Future<List<Quote>> getQuoteByCatId(String catId, int current_page) async {
   final response =
-      await http.get('$fakemainUrl/get_quote_by_cat_id_2/$catId/$current_page');
+      await http.get('$mainUrl/get_quote_by_cat_id_2/$catId/$current_page');
   return quoteFromJson(response.body);
 }
 
 Future<List<Forum>> getAllForums() async {
-  final response = await http.get('$fakemainUrl/get_forum');
+  final response = await http.get('$mainUrl/get_forum');
   return forumFromJson(response.body);
 }
 
 Future<List<Forum>> getAllForumPerPage(int current_page) async {
-  final response = await http.get('$fakemainUrl/get_forum_2/$current_page');
+  final response = await http.get('$mainUrl/get_forum_2/$current_page');
   return forumFromJson(response.body);
 }
 
 Future<List<ForumComment>> getForumCommentById(String id) async {
-  final response = await http.get('$fakemainUrl/get_forum_comment_by_id/$id');
+  final response = await http.get('$mainUrl/get_forum_comment_by_id/$id');
   return forumCommentFromJson(response.body);
 }
 
 Future<List<ForumComment>> getForumCommentByIdnPerPage(
     String id, int current_page) async {
-  final response = await http
-      .get('$fakemainUrl/get_forum_comment_by_id_2/$id/$current_page');
+  final response =
+      await http.get('$mainUrl/get_forum_comment_by_id_2/$id/$current_page');
   return forumCommentFromJson(response.body);
 }
 
@@ -62,7 +62,7 @@ Future<bool> makeCommentPost(
     String id, String name, String email, String comment) async {
   //return forumCommentFromJson(response.body);
 
-  final uri = Uri.parse('$fakemainUrl/make_comment');
+  final uri = Uri.parse('$mainUrl/make_comment');
   var request = http.MultipartRequest('POST', uri);
   request.fields['name'] = name;
   request.fields['email'] = email;
@@ -81,7 +81,7 @@ Future<bool> makePost(String name, String email, String title, String comment,
     File postImg, File profileImg) async {
   //return forumCommentFromJson(response.body);
 
-  final uri = Uri.parse('$fakemainUrl/make_post');
+  final uri = Uri.parse('$mainUrl/make_post');
   var request = http.MultipartRequest('POST', uri);
   request.fields['title'] = title;
   request.fields['body'] = comment;
@@ -104,7 +104,7 @@ Future<bool> makePost(String name, String email, String title, String comment,
 }
 
 Future<List<Forum>> searchForumPost(String search_term) async {
-  final uri = Uri.parse('$fakemainUrl/search_forum');
+  final uri = Uri.parse('$mainUrl/search_forum');
 
   var response = await http.post(uri, body: {
     'search_term': search_term,
@@ -115,7 +115,7 @@ Future<List<Forum>> searchForumPost(String search_term) async {
 
 Future<List<Forum>> searchForumPostByPage(
     String search_term, int current_page) async {
-  final uri = Uri.parse('$fakemainUrl/search_forum_by_page/$current_page');
+  final uri = Uri.parse('$mainUrl/search_forum_by_page/$current_page');
 
   var response = await http.post(uri, body: {
     'search_term': search_term,
